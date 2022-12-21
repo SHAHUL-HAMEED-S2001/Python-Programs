af129c86a7826e3164b5f588398519d2a6249105
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2022-12-21 15:55:40 +0530

    upd

--- Recursive Programming/Py_2.py
+++ Recursive Programming/Py_2.py
@@ -1,4 +1,5 @@
 # Python Program to Check if a Number is a Palindrome using Recursion
+
 class Program:
     def __init__(self):
         self.dup = 0
@@ -7,17 +8,14 @@ class Program:
         if num == 0:
             return
         
-        self.dup = self.dup*10 + (num%10)
-        
+        self.dup = self.dup*10 + (num%10)  
+              
         sol.checkPalindrome(num//10)
     
     def isPalindrome(self,num):
-        
         sol.checkPalindrome(num)
-        
         if (num == self.dup): return 1
         else: return 0
-
 
 val = int(input("Enter number to check for Palindrome: "))
 sol = Program()
